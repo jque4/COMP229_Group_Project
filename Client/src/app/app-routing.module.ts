478fa33704a0af8169946bcbd2cf64a42ec09e54
Alley <alleychaggar@gmail.com>
2022-11-26 22:00:39 -0500

    Added basepage and deleted productpage

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { ProductsComponent } from './pages/products/products.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent, data: {title: 'Home'}},
   {path: 'about', component: AboutComponent, data: {title: 'About'}},
-  {path: 'products', component: ProductsComponent, data: {title: 'Products'}},
   {path: 'contact', component: ContactComponent, data: {title: 'Contact'}},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { BasePageComponent } from './partials/base-page/base-page.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ProductsComponent } from './pages/products/products.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    ProductsComponent
+    ProductsComponent,
+    BasePageComponent
   ],
   imports: [
     BrowserModule,
