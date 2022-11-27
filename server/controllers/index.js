60ef01e8e84185f6ef79f3ffab9862793e8bc144
jque4 <jque4@my.centennialcollege.ca>
2022-11-26 20:41:44 -0500

    added authentication

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,11 @@
 let express = require('express');
 let router = express.Router();
+let mongoose = require('mongoose');
+let passport = require('passport');
+
+// create user model instance
+let userModel = require('../models/user');
+let User = userModel.User;
 
 module.exports.displayHomePage = (req, res, next) => {
     res.render('index', {title: 'Home'});
@@ -19,4 +25,80 @@ module.exports.displayServicesPage = (req, res, next) => {
 
 module.exports.displayContactPage = (req, res, next) => {
     res.render('index', { title: 'Contact'});
+}
+
+module.exports.displayLoginPage = (req, res, next) => {
+    if (!req.user) {
+        res.render('auth/login', { 
+            title: 'Login',
+            messages: req.flash('loginMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        });
+    } else {
+        return res.redirect('/');
+    }
+}
+
+module.exports.processLoginPage = (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        // server error
+        if (err) return next(err);
+        if (!user) {
+            req.flash('loginMessage', 'Error 401: Unauthorized');
+            return res.redirect('/login');
+        }
+        req.login(user, (err) => {
+            // server error (continued)
+            if (err) return next(err);
+            return res.redirect('/product-list');
+        })(req, res, next);
+    })
+}
+
+module.exports.displayRegisterPage = (req, res, next) => {
+    if (!req.user) {
+        res.render('auth/register', { 
+            title: 'Register',
+            messages: req.flash('registerMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        });
+    } else {
+        return res.redirect('/');
+    }
+}
+
+module.exports.processRegisterPage = (req, res, next) => {
+    // create new user object
+    let newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        displayName: req.body.displayName
+    });
+
+    User.register(newUser, req.body.password, (err) => {
+        if (err) { // On Registration Error
+            console.log("Error creating new user");
+            if (err.name == "UserExistsError") {
+                req.flash(
+                    'registerMessage',
+                    'That username is unavailable'
+                );
+                console.log('Registration Error: User already exists');
+            }
+            return res.render('auth/register', {
+                title: 'Register',
+                messages: req.flash('registerMessage'),
+                displayName: req.user ? req.user.displayName : ''
+            });
+        } else {
+            return passport.authenticate('local')(req, res, () => {
+                res.redirect('/product-list');
+            });
+        }
+    });
+}
+
+module.exports.performLogout = (req, res, next) => {
+    req.logout();
+    res.redirect('/');
 }
