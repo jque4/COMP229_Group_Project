d4f8a461bdf95f32f4b2255f5c09f318389766eb
jque4 <113262715+jque4@users.noreply.github.com>
2022-11-26 22:08:42 -0500

    added authentication

--- server/routes/index.js
+++ server/routes/index.js
@@ -21,4 +21,19 @@ router.get('/services', indexController.displayServicesPage);
 /* GET Contact Us page. */
 router.get('/contact', indexController.displayContactPage);
 
+/* GET Route for displaying the LOGIN page */
+router.get('/login', indexController.displayLoginPage);
+
+/* POST Route for processing the LOGIN page */
+router.post('/login', indexController.processLoginPage);
+
+/* GET Route for displaying the REGISTER page */
+router.get('/register', indexController.displayRegisterPage);
+
+/* POST Route for processing the REGISTER page */
+router.post('/register', indexController.processRegisterPage);
+
+/* GET Route for processing user logout  */
+router.get('/logout', indexController.performLogout);
+
 module.exports = router;
