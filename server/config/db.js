83ea1fb7170fe883eecd3f0e804e2760a59657fc
jque4 <113262715+jque4@users.noreply.github.com>
2022-12-03 18:51:04 -0500

    Fork from auth_1

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb+srv://admin:admin@mongodbserver.lkz5jhf.mongodb.net/?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://admin:admin@mongodbserver.lkz5jhf.mongodb.net/grocery_store?retryWrites=true&w=majority"
     // "URI": "mongodb://127.0.0.1/grocery_store"
 }
